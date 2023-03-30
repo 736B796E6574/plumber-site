c226f7f1c3c6567258c719e018c01ba38a5e90a1
martin-mcinerney <martin_mcinerney@live.ie>
2023-03-30 15:15:48 +0100

    where lambo?

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/Homepage';
 import Button from 'react-bootstrap/Button';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-    
+    <LoginButton />
+    <LogoutButton />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,20 +7,21 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Auth0Provider
-    domain="dev-hgglmipgygf5ikzv.us.auth0.com"
-    clientId="ddgHUCB8KtseTGDis7YZ9DKFGeWZxDRS"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
+  
+      <Auth0Provider
+  domain="dev-hgglmipgygf5ikzv.us.auth0.com"
+  clientId="ddgHUCB8KtseTGDis7YZ9DKFGeWZxDRS"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
   >
     <App />
   </Auth0Provider>
-  </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
