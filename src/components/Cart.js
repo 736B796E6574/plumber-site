8d9d795d5afa9651ab51cec569907e9c09159946
martin-mcinerney <martin_mcinerney@live.ie>
2023-04-27 22:59:01 +0100

    add cols to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,18 +9,19 @@ function Cart() {
           <Col className="d-flex flex-row cart-headers">
           <div className="d-flex flex-row cart-headers"><h1>My basket</h1><p className="item-count">1 Item</p></div>
           <p className="continue-p">⬅ <span className='continue-shopping'>Continue Shopping</span></p>
-          <Row>
-             
-          </Row>
-
          </Col>
         <Col>
-        
+        The other button
         </Col>
         </Row>
 <Row>
-
-    
+<Col>
+  <div className="bg-light">The product</div>
+</Col>
+<Col className="bg-light">
+  <div>The go to checkout summary</div>
+</Col>
+  
   </Row>
 
       </Container>
